632ba3e40291e45139ec14051297f6a11d4ee084
dldkfma9577 <arlee957790@gmail.com>
2022-04-26 19:04:57 +0900

    2022.04.26 오늘치 곱부

---
+++ Baekjoon/6. 2605) 줄세우기.cpp
@@ -0,0 +1,28 @@
+//#include <iostream>
+//#include <vector>
+//
+//using namespace std;
+//int N;
+//vector<int> list;
+//
+//void solution() {
+//	vector<int> tmp;
+//	for (int i = 0; i < list.size(); i++) {
+//		tmp.insert(tmp.begin() + (i-list[i]), i + 1);
+//	}
+//	for (int n : tmp) {
+//		cout << n << " ";
+//	}
+//	cout << endl;
+//}
+//
+//int main() {
+//	cin >> N;
+//	for (int i = 0; i < N; i++) {
+//		int n;
+//		cin >> n;
+//		list.push_back(n);
+//	}
+//	solution();
+//	return 0;
+//}
