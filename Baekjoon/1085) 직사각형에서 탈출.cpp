31927d1ef9daa8f3cf07e0151c448e8dd4f26af5
dldkfma9577 <arlee957790@gmail.com>
2022-06-01 23:41:00 +0900

    2022.06.01 오늘치 공부

---
+++ Baekjoon/1085) 직사각형에서 탈출.cpp
@@ -0,0 +1,11 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	int x, y, w, h;
+//	cin >> x >> y >> w >> h;
+//	int height = min(x, w - x);
+//	int width = min(y, h - y);
+//	cout << min(height, width) << endl;
+//	return 0;
+//}
