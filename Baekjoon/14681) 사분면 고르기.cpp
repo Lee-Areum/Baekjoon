7e7375bd69ddf6a562a34c188d5fb8e15f8cdacc
dldkfma9577 <arlee957790@gmail.com>
2022-06-19 12:14:47 +0900

    2022.06.19 오늘치 공부

---
+++ Baekjoon/14681) 사분면 고르기.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//using namespace std;
+//int main() {
+//	int n1, n2; cin >> n1 >> n2;
+//	if (n1 > 0) {
+//		if (n2 > 0) {
+//			cout << 1 << endl;
+//		}
+//		else {
+//			cout << 4 << endl;
+//		}
+//	}
+//	else {
+//		if (n2 > 0) {
+//			cout << 2 << endl;
+//		}
+//		else {
+//			cout << 3 << endl;
+//		}
+//	}
+//	return 0;
+//}
