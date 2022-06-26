e773e76ae644281ecf66d09769cbf93800023f9d
dldkfma9577 <arlee957790@gmail.com>
2022-06-26 19:03:06 +0900

    2022.06.26 오늘치 공부

---
+++ Baekjoon/15552) 빠른 A+B.cpp
@@ -0,0 +1,14 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	cin.tie(NULL);
+//	std::ios_base::sync_with_stdio(false);
+//	int T; cin >> T;
+//	for (int t = 1; t <= T; t++) {
+//		int n, m;
+//		cin >> n >> m;
+//		cout << n + m << "\n";
+//	}
+//	return 0;
+//}
