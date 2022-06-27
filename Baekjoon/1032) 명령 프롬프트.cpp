f0ea4ccca4dc1654b8820022b645f4ac677bbd8b
dldkfma9577 <arlee957790@gmail.com>
2022-06-27 13:20:58 +0900

    2022.06.27 오늘치 공부

---
+++ Baekjoon/1032) 명령 프롬프트.cpp
@@ -0,0 +1,17 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//int main() {
+//	int N; cin >> N;
+//	string answer; cin >> answer;
+//	for (int i = 1; i < N; i++) {
+//		string comp; cin >> comp;
+//		for (int k = 0; k < answer.size(); k++) {
+//			if (answer[k] != '?' && answer[k] != comp[k]) {
+//				answer[k] = '?';
+//			}
+//		}
+//	}
+//	cout << answer << endl;
+//	return 0;
+//}
