bb205cb3f34bec820dd80da9c4e781db80c100d3
dldkfma9577 <arlee957790@gmail.com>
2022-07-25 19:27:46 +0900

    22.07.25 오늘치 공부

---
+++ Baekjoon/1977) 완전제곱수.cpp
@@ -0,0 +1,23 @@
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main() {
+//	int N, M; cin >> N >> M;
+//	int sum = 0, minest = M;
+//	for (int i = N; i <= M; i++) {
+//		int n = sqrt(i);
+//		if (pow(n, 2) == i) {
+//			sum += i;
+//			minest = min(minest, i);
+//		}
+//	}
+//	if (sum <= 0) {
+//		cout << -1 << endl;
+//	}
+//	else {
+//		cout << sum << endl;
+//		cout << minest << endl;
+//	}
+//
+//	return 0;
+//}
