e1c28c75b3a8bbe23be7c74200f6d42e3b2033e7
이아름 <72062916+Lee-Areum@users.noreply.github.com>
2023-09-15 00:01:08 +0900

    Update 가장 가까운 같은 글자.cpp

--- Programmers/가장 가까운 같은 글자.cpp
+++ Programmers/가장 가까운 같은 글자.cpp
@@ -1,1 +1,19 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
 
+using namespace std;
+
+vector<int> solution(string s) {
+    vector<int> answer;
+    unordered_map<char,int> m;
+    for(int i = 0;i<s.size();i++){
+        if(m[s[i]]){
+            answer.push_back(i-m[s[i]]+1);
+        }else{
+            answer.push_back(-1);
+        }
+        m[s[i]] = i+1;
+    }
+    return answer;
+}
